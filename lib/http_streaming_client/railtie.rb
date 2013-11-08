befd2ac7e80c5d7de8a30948d0e736eadd21a749
David Tompkins <tompkins@adobe.com>
2013-11-07 16:47:48 -0800

    updated railtie

--- lib/http_streaming_client/railtie.rb
+++ lib/http_streaming_client/railtie.rb
@@ -2,7 +2,8 @@ require 'http_streaming_client'
 
 module HttpStreamingClient
   class Railties < ::Rails::Railtie
-    initializer 'Rails logger' do
+    initializer 'railties.configure_rails_initialization' do
+      puts "RAILTIE CALLED"
       HttpStreamingClient.logger = Rails.logger
     end
   end
--- lib/http_streaming_client/version.rb
+++ lib/http_streaming_client/version.rb
@@ -1,3 +1,3 @@
 module HttpStreamingClient
-  VERSION = "0.7.1"
+  VERSION = "0.7.2"
 end
