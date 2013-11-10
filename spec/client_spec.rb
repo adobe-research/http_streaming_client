940052ad7cc81592496def7547fb1b1ab1e4fba8
David Tompkins <tompkins@adobe.com>
2013-11-09 19:44:14 -0800

    added unit tests for client, post and get client instance requests

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -9,6 +9,14 @@ describe HttpStreamingClient do
     it { should_not be_nil}
   end
 
+  describe "client instance get test" do
+    client = HttpStreamingClient::Client.new
+    response = client.get "http://www.google.com/"
+    logger.debug "response: #{response}"
+    subject { response }
+    it { should_not be_nil}
+  end
+
   describe "static get test HTTPS" do
     response = HttpStreamingClient::Client.get("https://www.google.com/") { |chunk| logger.debug "got a chunk" }
     logger.debug "response: #{response}"
@@ -25,6 +33,18 @@ describe HttpStreamingClient do
   describe "static post test" do
     response = HttpStreamingClient::Client.post "http://posttestserver.com/post.php", "v=1.0&rsz=large&hl=en&geo=25187&key=ABQIAAAAh-n5SAB-cUnY3DufmfhdwBQuvo9pmDsxzxSHtaSRC_4ezr2lsRTOljpJVo81DJYBcnI00Fwk9xTdWQ"
     logger.debug "response: #{response}"
+    subject { response }
+    it { should_not be_nil}
+  end
+  
+  describe "client instance post test" do
+    client = HttpStreamingClient::Client.new
+    params = Hash.new
+    params['v'] = "1.0"
+    params['rsz'] = "large"
+    params['hl'] = "geo"
+    params['geo'] = "25187"
+    response = client.post "http://posttestserver.com/post.php", params
     subject { response }
     it { should_not be_nil}
   end
